e8900c00c51a02b4522a6c1232837cc76990cfe8
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-22 15:29:35 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -5,21 +5,26 @@
 #include <linux/syscalls.h>
 #include <linux/sched.h>
 
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.01");
+
 int pid = 0;
 module_param(pid, int, 0);
 
-static int __init init(void)
+static int __init q3_init(void)
 {
-    struct task_struct *t;
-    t = pid_task(find_vpid(pid), PIDTYPE_PID);
-    if (!t)
-    {
-        return -ESRCH;
-    }
-    printk(KERN_INFO "Pid: %d\n", t1->pid);
-    printk(KERN_INFO "Uid: %d\n", t1->cred->uid.val);
-    printk(KERN_INFO "Pgid: %d\n", t1->group_leader->pid);
-    printk(KERN_INFO "Comm: %s\n", t1->comm);
+    struct task_struct *t1;
+    t1 = pid_task(find_vpid(pid), PIDTYPE_PID);
+   
+    printk(KERN_INFO "The pid is: %d\n", t1->pid);
+    printk(KERN_INFO "The uid is: %d\n", t1->cred->uid.val);
+    printk(KERN_INFO "The pgid is: %d\n", t1->group_leader->pid);
+    printk(KERN_INFO "The comm is: %s\n", t1->comm);
     return 0;
 }
-module_init(init);
+static void __exit q3_exit(void)
+{
+    printk(KERN_INFO "Bye\n");
+}
+module_init(q3_init);
+module_exit(q3_exit);
