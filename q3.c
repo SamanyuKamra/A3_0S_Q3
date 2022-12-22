92623a3937bae328b1f5b0142363faf310b62c94
SamanyuKamra <108547482+SamanyuKamra@users.noreply.github.com>
2022-12-22 15:23:36 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -1,37 +1,25 @@
-#include <linux/kernel.h>
-#include <linux/unistd.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/sched.h>
+#include <linux/kernel.h>
+#include <linux/unistd.h>
 #include <linux/syscalls.h>
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Samanyu Kamra - 2021487");
-MODULE_DESCRIPTION("Linux module for reading process as entries.");
-MODULE_VERSION("0.01");
+#include <linux/sched.h>
 
 int pid = 0;
-module_param(pid,int,0);
+module_param(pid, int, 0);
 
-static int __init q3_init(void)
+static int __init init(void)
 {
- struct task_struct *process;
- pid_t process_pid ;
- uid_t process_uid ;
- pid_t process_pgid;
- 
- process = pid_task(find_vpid(pid), PIDTYPE_PID);
- process_pid = process->pid;
- process_uid = process->cred->uid.val;
- process_pgid = process->group_leader->pid;
- printk(KERN_INFO "comm: %s\n",process->comm);
- printk(KERN_INFO "pid: %d\n",process_pid);
- printk(KERN_INFO "uid: %d\n",process_uid);
- printk(KERN_INFO "pgid: %d\n",process_pgid);
- return 0;
+    struct task_struct *t;
+    t = pid_task(find_vpid(pid), PIDTYPE_PID);
+    if (!t)
+    {
+        return -ESRCH;
+    }
+    printk(KERN_INFO "Pid: %d\n", t1->pid);
+    printk(KERN_INFO "Uid: %d\n", t1->cred->uid.val);
+    printk(KERN_INFO "Pgid: %d\n", t1->group_leader->pid);
+    printk(KERN_INFO "Comm: %s\n", t1->comm);
+    return 0;
 }
-static void __exit q3_exit(void) {
- printk(KERN_INFO "Module ended\n");
-}
-module_init(q3_init);
-module_exit(q3_exit);
+module_init(init);
